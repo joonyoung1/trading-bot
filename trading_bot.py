bb9e8be22f0836a27df84ad1d9044c29fa377663
joonyoung1 <joonyoung1023@gmail.com>
2024-08-30 19:38:42 +0900

    Refactor structure - Implement Manager and Broker - decouple trading_bot and price_fetcher

--- trading_bot.py
+++ trading_bot.py
@@ -1,110 +1,86 @@
-import os
-import time
-import logging
-from logging.handlers import RotatingFileHandler
-from dotenv import load_dotenv
-
-load_dotenv()
-
-import pyupbit
-from pyupbit import WebSocketManager
-
-
-ACCESS = os.getenv("ACCESS")
-SECRET = os.getenv("SECRET")
-TICKER = os.getenv("TICKER")
+from broker import Broker
+from logging import Logger
+from multiprocessing import Queue
 
 
 class TradingBot:
-    def __init__(self):
-        self.upbit = pyupbit.Upbit(ACCESS, SECRET)
+    SENTINEL = object()
+
+    def __init__(
+        self, ticker: str, queue: Queue, broker: Broker, logger: Logger
+    ) -> None:
+        self.ticker: str = ticker
+        self.queue: Queue = queue
+        self.broker: Broker = broker
+        self.logger: Logger = logger
+
+        self.cash: float = 0
+        self.quantity: float = 0
+
         self.update_balance()
-        self.cancel_orders()
-        self.init_logger()
+        self.broker.cancel_orders(self.ticker)
+        self.logger.info("Trading bot initialized.")
+    
+    def start(self) -> None:
+        self.logger.info("Trading bot started.")
+        last_price = -1
 
-    def init_logger(self):
-        self.logger = logging.getLogger("logger")
-        self.logger.setLevel(logging.DEBUG)
-
-        handler = RotatingFileHandler(
-            "logfile.log", maxBytes=1024 * 1024 * 5, backupCount=3
-        )
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-
-    def run(self):
-        wm = WebSocketManager("ticker", [TICKER])
         while True:
-            data = wm.get()
+            data = self.queue.get()
+            if data is self.SENTINEL:
+                break
+            
             price = data["trade_price"]
-            ret = self.action(price)
+            if last_price != price:
+                last_price = price
+                self.process_trade(price)
 
-            if ret:
-                self.update_balance()
+    def terminate(self) -> None:
+        self.logger.info("Trading bot terminated.")
+        self.queue.put(self.SENTINEL)
 
-    def action(self, price):
-        asset_value = price * self.quantity
-        total_value = self.cash + asset_value
-
-        volume = abs(self.cash - asset_value) / 2
-        if volume < 5001:
-            return False
-
-        ratio = asset_value / total_value
-        if ratio < 0.5:
-            ret = self.buy(price, volume / price)
-        elif ratio > 0.505:
-            ret = self.sell(price, volume / price)
-
-        return ret
-
-    def buy(self, price, quantity):
-        order = self.upbit.buy_limit_order(TICKER, price, quantity)
-        uuid = order["uuid"]
-
-        self.logger.info(f"Buy {quantity} at {price} (₩{price * quantity}).")
-        self.logger.info(f"Order [{uuid}] opened.")
-
-        return self.wait(uuid)
-
-    def sell(self, price, quantity):
-        order = self.upbit.sell_limit_order(TICKER, price, quantity)
-        uuid = order["uuid"]
-
-        self.logger.info(f"Sell {quantity} at {price} (₩{price * quantity}).")
-        self.logger.info(f"Order [{uuid}] opened.")
-
-        return self.wait(order)
-
-    def wait(self, uuid):
-        for _ in range(30):
-            order = self.upbit.get_order(uuid)
-            if order["state"] == "done":
-                self.logger.info(f"Order [{uuid}] has been successfully closed.")
-                return True
-            time.sleep(10)
-
-        self.cancel_orders()
-        return False
-
-    def update_balance(self):
-        self.cash = self.upbit.get_balance("KRW")
-        self.quantity = self.upbit.get_balance(TICKER)
+    def update_balance(self) -> None:
+        self.cash = self.broker.get_balance("KRW")
+        self.quantity = self.broker.get_balance(self.ticker)
         self.logger.info(f"Cash: ₩{self.cash}, Assets: {self.quantity} units.")
 
-    def cancel_orders(self):
-        self.logger.info("Closing all open orders...")
-        
-        orders = self.upbit.get_order(TICKER)
-        for order in orders:
-            uuid = order["uuid"]
-            self.upbit.cancel_order(uuid)
-        
-        self.logger.info("All open orders have been successfully closed.")
+    def process_trade(self, price: float) -> None:
+        print("price", price)
+        asset_value = price * self.quantity
+        volume = abs(self.cash - asset_value) / 2
+        print("volume", volume)
+        if volume < 5001:
+            return
 
-if __name__ == "__main__":
-    trading_bot = TradingBot()
-    trading_bot.run()
+        ratio = asset_value / asset_value + self.cash
+        if ratio < 0.5:
+            self.buy(price, volume / price)
+        elif ratio > 0.505:
+            self.sell(price, volume / price)
+
+    def buy(self, price: float, quantity: float) -> None:
+        self.logger.info(f"Buy {quantity} at {price} (₩{price * quantity}).")
+
+        order = self.broker.buy_limit_order(self.ticker, price, quantity)
+        uuid = order["uuid"]
+        self.logger.info(f"Order [{uuid}] opened.")
+
+        self.wait(uuid)
+
+    def sell(self, price: float, quantity: float) -> None:
+        self.logger.info(f"Sell {quantity} at {price} (₩{price * quantity}).")
+
+        order = self.broker.sell_limit_order(self.ticker, price, quantity)
+        uuid = order["uuid"]
+        self.logger.info(f"Order [{uuid}] opened.")
+
+        self.wait(uuid)
+    
+    def wait(self, uuid: str) -> None:
+        closed = self.broker.wait_order_close(uuid)
+        if closed:
+            self.logger.info(f"Order [{uuid}] has been closed.")
+        else:
+            self.logger.info(f"Canceling all open orders...")
+            self.broker.cancel_orders(self.ticker)
+            self.logger.info(f"All open orders have been canceled.")
