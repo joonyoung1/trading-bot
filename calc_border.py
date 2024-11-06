34c499b5db865ce1f630fa9fc17f49e5c0cdcc67
joonyoung1 <joonyoung1023@gmail.com>
2024-11-06 12:58:23 +0900

    feat: calculate lower and upper bound from current price

---
+++ calc_border.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def calc_volume(initial_price, quantity, cash, current_price):
+    delta = current_price / initial_price - 1
+    if delta == 0:
+        ratio = 0.5
+    elif delta < 0:
+        ratio = 0.5 * delta**2 + delta + 0.5
+    else:
+        ratio = -0.5 * 2**-delta + 1
+
+    total_value = quantity * current_price + cash
+    proper_cash = total_value * ratio
+    return cash - proper_cash
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: python script_name.py <initial_price> <pivot> <quantity> <cash>")
+        sys.exit(1)
+
+    initial_price = float(sys.argv[1])
+    pivot = float(sys.argv[2])
+    quantity = float(sys.argv[3])
+    cash = float(sys.argv[4])
+
+    lower_price = pivot
+    while True:
+        lower_price = ((lower_price * 10) - 1) / 10
+        volume = calc_volume(initial_price, quantity, cash, lower_price)
+        if abs(volume) > 5000:
+            break
+
+    print(f"lower_price : {lower_price}")
+
+    upper_price = pivot
+    while True:
+        upper_price = ((upper_price * 10) + 1) / 10
+        volume = calc_volume(initial_price, quantity, cash, upper_price)
+        if abs(volume) > 5000:
+            break
+
+    print(f"upper_price : {upper_price}")
