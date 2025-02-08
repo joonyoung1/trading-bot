db7481e11644853cff7be498cf5188848b04f698
joonyoung1 <joonyoung1023@gmail.com>
2025-02-08 17:24:32 +0900

    feat: implement get_balances

--- app/broker.py
+++ app/broker.py
@@ -1,96 +1,53 @@
 import os
-import asyncio
-from functools import partial
+import jwt
+import hashlib
+import uuid
 
-import pyupbit
+import aiohttp
+from urllib.parse import urljoin, urlencode, unquote
 
-from utils import retry
 from schemas import ConfigKeys
 
 
 class Broker:
-    def __init__(self) -> None:
-        access = os.getenv(ConfigKeys.ACCESS)
-        secret = os.getenv(ConfigKeys.SECRET)
-        self.upbit = pyupbit.Upbit(access, secret)
+    def __init__(self):
+        self.ACCESS = os.getenv(ConfigKeys.ACCESS)
+        self.SECRET = os.getenv(ConfigKeys.SECRET)
 
-    def set_loop(self, loop: asyncio.AbstractEventLoop) -> None:
-        self.loop = loop
+        self.base_url = "https://api.upbit.com"
+        self.session = aiohttp.ClientSession()
 
-    @retry()
-    async def get_current_price(self, ticker) -> float:
-        task = partial(pyupbit.get_current_price, ticker)
-        price = await self.loop.run_in_executor(None, task)
-        return float(price)
+    async def request(self, method: str, url: str, **kwargs) -> dict:
+        async with self.session.request(method=method, url=url, **kwargs) as response:
+            return await response.json()
 
-    @retry()
-    async def get_balances(self) -> dict:
-        task = partial(self.upbit.get_balances)
-        return await self.loop.run_in_executor(None, task)
+    async def get_balances(self):
+        payload = {
+            "access_key": self.ACCESS,
+            "nonce": str(uuid.uuid4()),
+        }
+        headers = {
+            "Authorization": f"Bearer {jwt.encode(payload, self.SECRET)}",
+        }
 
-    @retry()
-    async def get_balance(self, ticker: str) -> float:
-        task = partial(self.upbit.get_balance, ticker)
-        balance = await self.loop.run_in_executor(None, task)
+        url = urljoin(self.base_url, "/v1/accounts")
+        return await self.request("GET", url, headers=headers)
 
-        if balance is None:
-            raise ValueError("Balance is `None` instead of a float")
-        return float(balance)
 
-    @retry()
-    async def buy_limit_order(self, ticker: str, price: float, quantity: float) -> dict:
-        task = partial(self.upbit.buy_limit_order, ticker, price, quantity)
-        return await self.loop.run_in_executor(None, task)
 
-    @retry()
-    async def sell_limit_order(
-        self, ticker: str, price: float, quantity: float
-    ) -> dict:
-        task = partial(self.upbit.sell_limit_order, ticker, price, quantity)
-        return await self.loop.run_in_executor(None, task)
+    async def close(self):
+        await self.session.close()
 
-    @retry()
-    async def buy_market_order(self, ticker: str, amount: float) -> dict:
-        task = partial(self.upbit.buy_market_order, ticker, amount)
-        return await self.loop.run_in_executor(None, task)
 
-    @retry()
-    async def sell_market_order(self, ticker: str, quantity: float) -> dict:
-        task = partial(self.upbit.sell_market_order, ticker, quantity)
-        return await self.loop.run_in_executor(None, task)
+if __name__ == "__main__":
+    import asyncio
 
-    @retry()
-    async def check_order_closed(self, uuid: str) -> bool:
-        order = await self.get_order(uuid)
-        return order is not None and (
-            order["state"] == "done" or order["state"] == "cancel"
-        )
+    async def main():
+        broker = Broker()
 
-    @retry()
-    async def get_order(self, uuid_or_ticker: str) -> dict:
-        task = partial(self.upbit.get_order, uuid_or_ticker)
-        return await self.loop.run_in_executor(None, task)
+        res = await broker.get_balances()
+        print(res)
 
-    @retry()
-    async def cancel_orders(self, ticker: str) -> None:
-        response = await self.get_order(ticker)
-        if isinstance(response, dict) and "error" in response:
-            error = response["error"]
-            error_name = error.get("name", "Unknown error")
-            error_message = error.get("message", "No error message provided")
-            raise ConnectionError(f"Error: {error_name} - {error_message}")
+        await broker.close()
 
-        for order in response:
-            uuid = order["uuid"]
-            await self.cancel_order(uuid)
-
-        for order in response:
-            uuid = order["uuid"]
-
-            while not await self.check_order_closed(uuid):
-                await asyncio.sleep(0.5)
-
-    @retry()
-    async def cancel_order(self, uuid: str) -> None:
-        task = partial(self.upbit.cancel_order, uuid)
-        await self.loop.run_in_executor(None, task)
+    asyncio.run(main())
