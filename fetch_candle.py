b904d859edae3de395d66fef7c52065cb17c0fac
joonyoung1 <joonyoung1023@gmail.com>
2025-02-11 00:17:52 +0900

    feat: find optimal price gap using bayesian optimization

--- fetch_candle.py
+++ fetch_candle.py
@@ -1,45 +1,36 @@
 import asyncio
-import pandas as pd
 from datetime import datetime, timezone, timedelta
-from urllib.parse import urljoin
 
 from app.broker import Broker
 
-
-if __name__ == "__main__":
+async def fetch_price_series():
     now = datetime.now(timezone.utc)
 
-    async def main():
-        try:
-            fetcher = Broker()
-            fetcher.initialize()
+    try:
+        fetcher = Broker()
+        fetcher.initialize()
 
-            tos = [
-                (now - timedelta(minutes=144 * x)).strftime("%Y-%m-%d %H:%M:%S")
-                for x in range(70)
-            ]
+        tos = [
+            (now - timedelta(minutes=144 * x)).strftime("%Y-%m-%d %H:%M:%S")
+            for x in range(70)
+        ]
 
-            tasks = [fetcher.get_candles(to, 144, "minute", 1) for to in tos]
+        tasks = [fetcher.get_candles(to, 144, "minute", 1) for to in tos]
 
-            results = await asyncio.gather(*tasks)
+        results = await asyncio.gather(*tasks)
 
-            data = []
-            for candles in results[::-1]:
-                for candle in candles[::-1]:
-                    data.append(
-                        {
-                            "timestamp": candle.candle_date_time_utc,
-                            "o": candle.opening_price,
-                            "h": candle.high_price,
-                            "l": candle.low_price,
-                            "t": candle.trade_price,
-                        }
+        price_series = [results[-1][-1].opening_price]
+        for candles in results[::-1]:
+            for candle in candles[::-1]:
+                if candle.opening_price < candle.trade_price:
+                    price_series.extend(
+                        [candle.low_price, candle.high_price, candle.trade_price]
+                    )
+                else:
+                    price_series.extend(
+                        [candle.high_price, candle.low_price, candle.trade_price]
                     )
 
-            return pd.DataFrame(data)
-        finally:
-            await fetcher.close()
-
-    data = asyncio.run(main())
-    data["timestamp"] = pd.to_datetime(data["timestamp"])
-    data.to_csv("xrp_24h.csv", index=False)
+        return price_series
+    finally:
+        await fetcher.close()
--- simulate.py
+++ simulate.py
@@ -1,69 +1,68 @@
-import pandas as pd
-import numpy as np
+import asyncio
+
+from bayes_opt import BayesianOptimization
+
+from fetch_candle import fetch_price_series
+from app.utils import calc_ratio
 
 
-def calc_ratio(price: float, pivot_price: float):
-    if price >= pivot_price:
-        delta = (price / pivot_price) - 1
-        ratio = -0.5 * (2**-delta) + 1
-    else:
-        delta = (pivot_price / price) - 1
-        ratio = 0.5 * (2**-delta)
-    return np.clip(ratio, 0.125, 0.875)
-
-
-data = pd.read_csv("xrp_24h.csv")
-
-initial_price = data.iloc[0]["o"]
-initial_balance = 3000000
-t = 0.005
-
-
-for t in [0.003, 0.005, 0.01, 0.015, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2]:
-# for t in range(1, 10):
-    # t /= 100
-    # pivot_price = initial_price
-    pivot_price = 1609
+def simulate(t: float, price_series):
+    initial_price = price_series[0]
+    initial_balance = 10000000
+    pivot_price = initial_price
     last_trade_price = initial_price
-    last_price = initial_price
+
     cash = initial_balance / 2
     quantity = (initial_balance / 2) / initial_price
-
     trade_count = 0
 
-    for _, row in data.iterrows():
-        if last_price <= row["t"]:
-            prices = [row["o"], row["h"], row["l"], row["t"]]
-        else:
-            prices = [row["o"], row["l"], row["h"], row["t"]]
-        last_price = prices[-1]
+    for price in price_series:
+        while True:
+            if last_trade_price * (1 + t) <= price:
+                trading_price = last_trade_price * (1 + t)
+            elif last_trade_price * (1 - t) >= price:
+                trading_price = last_trade_price * (1 - t)
+            else:
+                break
 
-        for price in prices:
-            while True:
-                if last_trade_price * (1 + t) <= price:
-                    trading_price = last_trade_price * (1 + t)
-                elif last_trade_price * (1 - t) >= price:
-                    trading_price = last_trade_price * (1 - t)
-                else:
-                    break
+            if pivot_price >= trading_price * 3:
+                pivot_price = trading_price * 3
+            elif pivot_price < trading_price / 3:
+                pivot_price = trading_price / 3
 
-                if pivot_price >= trading_price * 3:
-                    pivot_price = trading_price * 3
-                elif pivot_price < trading_price / 3:
-                    pivot_price = trading_price / 3
+            ratio = calc_ratio(trading_price, pivot_price)
 
-                ratio = calc_ratio(trading_price, pivot_price)
-                
-                volume = cash - (cash + quantity * trading_price) * ratio
+            volume = cash - (cash + quantity * trading_price) * ratio
 
-                cash -= volume
-                quantity += volume / trading_price
-                cash -= volume * 0.0005
+            cash -= volume
+            quantity += volume / trading_price
+            cash -= volume * 0.0005
 
-                last_trade_price = trading_price
-                trade_count += 1
+            last_trade_price = trading_price
+            trade_count += 1
 
-    # print(f"price: {price_series[-1]}")
-    print(f"t: {t}")
-    print(f"balance: {cash + quantity * data.iloc[-1]["t"]}")
-    print(f"trade_count: {trade_count}")
+    return cash + quantity * price_series[-1]
+
+
+async def main():
+    price_series = await fetch_price_series()
+
+    def objective_function(t):
+        # t = np.exp(log_t)
+        return simulate(t, price_series)
+
+    optimizer = BayesianOptimization(
+        f=objective_function,
+        pbounds={"t": (0.003, 0.05)},
+        random_state=42,
+        verbose=2,
+    )
+
+    optimizer.maximize(init_points=5, n_iter=25)
+
+    print(f"best_t: {optimizer.max["params"]["t"]}")
+    print(f"best_balance: {optimizer.max["target"]}")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
